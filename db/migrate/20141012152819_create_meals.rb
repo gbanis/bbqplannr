3a958dcc62c8ee20378a60af18e7ebbdec09c229
George <gbanis@gmail.com>
2014-10-12 11:32:56 -0400

    Meals and Bringitems belong_to Event

--- db/migrate/20141012152819_create_meals.rb
+++ db/migrate/20141012152819_create_meals.rb
@@ -2,6 +2,7 @@ class CreateMeals < ActiveRecord::Migration
   def change
     create_table :meals do |t|
       t.string :name
+      t.belongs_to :event, index: true
     end
   end
 end
--- db/migrate/20141012152900_create_bringitems.rb
+++ db/migrate/20141012152900_create_bringitems.rb
@@ -2,6 +2,7 @@ class CreateBringitems < ActiveRecord::Migration
   def change
     create_table :bringitems do |t|
       t.string :name
+      t.belongs_to :event, index: true
     end
   end
 end
